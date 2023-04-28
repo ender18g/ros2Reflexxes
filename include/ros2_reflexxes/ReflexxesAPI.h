d6180067f9e08fbbf43292cd1fcb0368c0c18c62
allan <allan.elsberry@gmail.com>
2023-04-28 19:57:49 -0400

    modified lib imports

--- include/ros2_reflexxes/ReflexxesAPI.h
+++ include/ros2_reflexxes/ReflexxesAPI.h
@@ -43,13 +43,13 @@
 #define __ReflexxesAPI__
 
 
-#include <RMLPositionInputParameters.h>
-#include <RMLPositionOutputParameters.h>
-#include <RMLPositionFlags.h>
-#include <RMLVelocityInputParameters.h>
-#include <RMLVelocityOutputParameters.h>
-#include <RMLVelocityFlags.h>
-#include <RMLVector.h>
+#include "RMLPositionInputParameters.h"
+#include "RMLPositionOutputParameters.h"
+#include "RMLPositionFlags.h"
+#include "RMLVelocityInputParameters.h"
+#include "RMLVelocityOutputParameters.h"
+#include "RMLVelocityFlags.h"
+#include "RMLVector.h"
 
 
 //  ---------------------- Doxygen info ----------------------
